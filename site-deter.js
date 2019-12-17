f8c33b3a9443dd0d47af7bb9eb517f88efe107b5
Rockwood Frank <43915594+rfrank6023@users.noreply.github.com>
2019-12-17 08:51:40 -0700

    Add js file

---
+++ site-deter.js
@@ -0,0 +1,2 @@
+if (window.location.href == "http://blueocean-iptv.com/rfrankweb.com/")
+	document.body.innerHTML = "We're sorry, but this site is currently down due to a DNS error. We are working to fix this issue."
