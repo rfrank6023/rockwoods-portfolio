eb38d71c81a7ce467f4c0202f05e97f584198c89
Rockwood Frank <43915594+rfrank6023@users.noreply.github.com>
2019-12-27 09:52:40 -0700

    Completely REDO with gridsome

--- site-deter.js
+++
@@ -1,2 +0,0 @@
-if (window.location.href == "http://blueocean-iptv.com/rfrankweb.com/")
-	document.body.innerHTML = "We're sorry, but this site is currently down due to a DNS error. We are working to fix this issue."
